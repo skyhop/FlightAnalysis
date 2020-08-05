805f4ed8316a516ecf34fb5f256f422d36edf43a
Corstian Boerman <mail@corstianboerman.com>
2020-08-05 11:07:02 +0200

    Move NormalizeData method together with the geo helpers
    After all they normalize position information

--- Skyhop.FlightAnalysis/Internal/Geo.cs
+++ Skyhop.FlightAnalysis/Internal/Geo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using NetTopologySuite.Geometries;
+using Skyhop.FlightAnalysis.Models;
 
 namespace Skyhop.FlightAnalysis.Internal
 {
@@ -94,6 +96,43 @@ namespace Skyhop.FlightAnalysis.Internal
             {
                 return 360 - absDiff;
             }
+        }
+
+        // ToDo: Add information about the aircrafts climbrate and so on, if possible
+        internal static PositionUpdate NormalizeData(FlightContext context, PositionUpdate position)
+        {
+            if (position == null) return position;
+
+            if (context.Flight.PositionUpdates.Count < 2
+                || !double.IsNaN(position.Heading) && !double.IsNaN(position.Speed)) return position;
+
+            var previousPosition =
+                context.Flight.PositionUpdates.LastOrDefault();
+
+            if (previousPosition == null) return position;
+
+            double? heading = null;
+            double? speed = null;
+
+            if (double.IsNaN(position.Heading)) heading = Geo.DegreeBearing(previousPosition.Location, position.Location);
+
+            if (double.IsNaN(position.Speed))
+            {
+                // 1. Get the distance (meters)
+                // 2. Calculate the time difference (seconds)
+                // 3. Convert to knots (1.94384449 is a constant)
+                var distance = previousPosition.Location.DistanceTo(position.Location);
+                var timeDifference = (position.TimeStamp - previousPosition.TimeStamp).Milliseconds;
+
+                if (timeDifference == 0) return null;
+
+                if (distance != 0) speed = distance / (timeDifference / 1000) * 1.94384449;
+            }
+
+            position.Speed = speed ?? position.Speed;
+            position.Heading = heading ?? position.Heading;
+
+            return position;
         }
     }
 }
